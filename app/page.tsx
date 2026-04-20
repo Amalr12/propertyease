594a7ee18c048cdcbc3870cc7ba108e32345a59e
Amalr12 git config --global user.name Amalr12 <amaldaspr12@gmail.com>
2026-04-20 14:34:21 +0530

    chore : push dev

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  < h1 className="text-3xl font-bold text-red-500">Hello World</h1>
+  < h1 className="text-3xl font-bold text-red-500">Hello</h1>
   );
 }
