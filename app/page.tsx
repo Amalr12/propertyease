00e9a848f27bd25a683740dcb57ac998bbe26172
Amalr12 git config --global user.name Amalr12 <amaldaspr12@gmail.com>
2026-05-07 18:55:07 +0530

    fix : build errors

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,14 @@ import Services from "./components/pages/home/Services";
 import WhyChooseUs from "./components/pages/home/WhyChooseUs";
 import Featured from "./components/pages/home/Featured";
 import ClientSay from "./components/pages/home/ClientSay";
+import { Suspense } from "react";
 
 
 export default function Home() {
   return (
     <>
-     <div className="">
-        <Hero/>
+     <div>
+        <Suspense fallback={<div>Loading...</div>}><Hero/></Suspense>
         <AboutUs/>
         <Values/>
         <Services/>
